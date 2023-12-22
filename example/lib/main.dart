640a16500b7a6fa68f676ce97316ec0f3b53f879
chenhongchen <412130100@qq.com>
2023-12-22 11:10:30 +0800

    添加例子

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,8 +1,6 @@
+import 'package:ext_gif/ext_image.dart';
+import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
-import 'dart:async';
-
-import 'package:flutter/services.dart';
-import 'package:ext_gif/ext_gif.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,29 +14,13 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
+  final String _gifUrl =
+      'http://img.mp.itc.cn/upload/20161107/5cad975eee9e4b45ae9d3c1238ccf91e.jpg';
+  bool _canMultiFrame = true;
 
   @override
   void initState() {
     super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {} on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {});
   }
 
   @override
@@ -48,8 +30,33 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Plugin example app'),
         ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+        body: Column(
+          children: [
+            SizedBox(height: MediaQuery.of(context).padding.top),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: ExtImage(
+                image: ExtendedNetworkImageProvider(_gifUrl),
+                fit: BoxFit.cover,
+                canMultiFrame: _canMultiFrame,
+              ),
+            ),
+          ],
+        ),
+        floatingActionButton: GestureDetector(
+          onTap: () {
+            _canMultiFrame = !_canMultiFrame;
+            setState(() {});
+          },
+          child: Container(
+            color: Colors.red,
+            width: 60,
+            height: 60,
+            child: const Icon(
+              Icons.switch_access_shortcut,
+              size: 40,
+            ),
+          ),
         ),
       ),
     );
